fc5c101f61e3802fb689bc2f86acdf02868ca87f
karthik96siddhu <karthik96siddhu@gmail.com>
2020-11-02 17:01:14 +0530

    app comp changes

--- word-generator/src/app/app.component.ts
+++ word-generator/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import arrayWords from "../utils/words";
+import arrayCountries from "../utils/countries";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  country = ''
+  limit = 10;
+
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(" ");
+  }
+
+  generateCountry() {
+    this.country = arrayCountries[Math.floor(Math.random() * 20)]
+  }
 }
